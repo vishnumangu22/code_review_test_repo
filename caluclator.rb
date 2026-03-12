e5426bc013eb7a0432c9d070ba79ee2965d1fbce
vishnumangu22 <vishnumangu22@gmail.com>
2026-03-12 11:58:37 +0530

    Updated Code

--- caluclator.rb
+++ caluclator.rb
@@ -1,26 +1,3 @@
-require 'httparty'
-
-# argument change + logic change
 def calculate(price)
   price
 end
-
-# method renamed
-def payment_process(user)
-  puts user
-end
-
-# method removed
-# send_email removed
-
-# method added
-def generate_invoice(order)
-  puts order
-end
-
-# logic changed
-def total_amount(a, b)
-  (a + b) * 2
-end
-
-
